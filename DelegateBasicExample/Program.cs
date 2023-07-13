a93a9751523a41329ae9d087284de75adf59e9de
huebner.christoph <huebner.christoph@t-online.de>
2023-07-13 18:30:09 +0200

    create delegate LogTextToScreen in the Program class to show the entered text with current date.

--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DelegateBasicExample
+{
+    class Program
+    {
+        delegate void LogDel(string text);
+        static void Main(string[] args)
+        {
+            LogDel logDel = new LogDel(LogTextToScreen);
+            Console.WriteLine("Please enter you name");
+            var name = Console.ReadLine();
+            if (name != null && name != "")
+            {
+                logDel(name);
+            }
+            else
+            {
+                Console.WriteLine("The name was empty!");
+            }
+        }
+
+        static void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
